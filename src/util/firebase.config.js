ce4df5e728e5170a4cdcdf895f2908fae20a93b9
jmconcha <jmconcha.work@gmail.com>
2021-03-20 14:45:09 +0800

    resolve firebase config issues

--- src/util/firebase.config.js
+++ src/util/firebase.config.js
@@ -2,7 +2,7 @@ const config = {
   apiKey: "AIzaSyB0S8j9gDaEMtBE2rhV-qFio8X-YXKCCCQ",
   authDomain: "blog-app-211e7.firebaseapp.com",
   projectId: "blog-app-211e7",
-  storageBucket: "gs://blog-app-211e7.appspot.com/",
+  storageBucket: "blog-app-211e7.appspot.com",
   messagingSenderId: "463100495595",
   appId: "1:463100495595:web:360cc940d011cd4dbfa0ae"
 };
